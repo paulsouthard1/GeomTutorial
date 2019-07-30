74dbbaaba5ac18d1455d0f340ef9b6efe47762ee
Paul Southard <39502694+paulsouthard1@users.noreply.github.com>
2019-07-30 16:31:19 -0500

    Update ChanEx.bat

--- ChanEx.bat
+++ ChanEx.bat
@@ -1,4 +1,4 @@
-start cmd.exe /k python C:\Users\ps29626\Documents\Anuga_Examiner\NoVeg_Flow.py lwc Bounds . 1 10 300 1 5 0.040 10 0.015
-start cmd.exe /k python C:\Users\ps29626\Documents\Anuga_Examiner\NoVeg_Flow.py lwc Bounds . 1 10 300 1 5 0.040 30 0.015
-start cmd.exe /k python C:\Users\ps29626\Documents\Anuga_Examiner\NoVeg_Flow.py lwc Bounds . 1 10 300 1 5 0.040 50 0.015
+start cmd.exe /k python NoVeg_Flow.py lwc Bounds . 1 10 300 1 5 0.040 10 0.015
+start cmd.exe /k python NoVeg_Flow.py lwc Bounds . 1 10 300 1 5 0.040 30 0.015
+start cmd.exe /k python NoVeg_Flow.py lwc Bounds . 1 10 300 1 5 0.040 50 0.015
 pause
